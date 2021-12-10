a6922f163cf37e654ef2d559787271bd3f482cee
Igor Mironchik <igor.mironchik@gmail.com>
2021-12-10 20:34:33 +0300

    Update read-excel/storage.hpp

--- read-excel/storage.hpp
+++ read-excel/storage.hpp
@@ -58,13 +58,13 @@ protected:
 
 struct EmptyStorage : public IStorage {
 protected:
-	virtual void onSharedString( size_t sstSize, size_t idx, const std::wstring & value ) override {}
-	virtual void onDateMode( uint16_t mode ) override {}
-	virtual void onSheet( size_t idx, const std::wstring & value ) override {}
-	virtual void onCellSharedString( size_t sheetIdx, size_t row, size_t column, size_t sstIndex ) override {}
-	virtual void onCell( size_t sheetIdx, size_t row, size_t column, const std::wstring & value ) override {}
-	virtual void onCell( size_t sheetIdx, size_t row, size_t column, double value ) override {}
-	virtual void onCell( size_t sheetIdx, const Formula & value ) override {}
+	void onSharedString( size_t sstSize, size_t idx, const std::wstring & value ) override {}
+	void onDateMode( uint16_t mode ) override {}
+	void onSheet( size_t idx, const std::wstring & value ) override {}
+	void onCellSharedString( size_t sheetIdx, size_t row, size_t column, size_t sstIndex ) override {}
+	void onCell( size_t sheetIdx, size_t row, size_t column, const std::wstring & value ) override {}
+	void onCell( size_t sheetIdx, size_t row, size_t column, double value ) override {}
+	void onCell( size_t sheetIdx, const Formula & value ) override {}
 }; // struct EmptyStorage
 
 } /* namespace Excel */
