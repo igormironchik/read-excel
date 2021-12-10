69eef9f60d9c20b1128751073357ea1e9193a4b4
Igor Mironchik <igor.mironchik@gmail.com>
2021-12-10 21:33:06 +0300

    Add some comments.

--- read-excel/storage.hpp
+++ read-excel/storage.hpp
@@ -45,17 +45,24 @@ namespace Excel {
 
 //! Excel storage interface.
 struct IStorage {
-	virtual ~IStorage() {}
+	virtual ~IStorage() = default;
 
 protected:
 	friend class Parser;
 
+	//! Handler of SST.
 	virtual void onSharedString( size_t sstSize, size_t idx, const std::wstring & value ) = 0;
+	//! Handler of date mode.
 	virtual void onDateMode( uint16_t mode ) = 0;
+	//! Handler of sheet.
 	virtual void onSheet( size_t idx, const std::wstring & value ) = 0;
+	//! Handler of cell with text from SST.
 	virtual void onCellSharedString( size_t sheetIdx, size_t row, size_t column, size_t sstIndex ) = 0;
+	//! Handler of cell with text.
 	virtual void onCell( size_t sheetIdx, size_t row, size_t column, const std::wstring & value ) = 0;
+	//! Handler of cell with double.
 	virtual void onCell( size_t sheetIdx, size_t row, size_t column, double value ) = 0;
+	//! Handler of cell with formula.
 	virtual void onCell( size_t sheetIdx, const Formula & value ) = 0;
 }; // struct IStorage
 
